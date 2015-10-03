003f79ec62bf40f4f2d920556bc19e7d9a515772
abejfehr <abe.fehr@gmail.com>
2015-10-03 12:07:32 -0400

    Display litres in result

--- pmt-dosage/src/pmtController.js
+++ pmt-dosage/src/pmtController.js
@@ -17,25 +17,27 @@
     $scope.binder = 340;
     $scope.loadSize = 5;
     $scope.pmt = 25;
+    $scope.litres = null;
     $scope.types = [
       {
         name: "Low",
-        value: 4/10
+        value: 4/1000
       },
       {
         name: "Medium",
-        value: 6/10
+        value: 6/1000
       },
       {
         name: "High",
-        value: 8/10
+        value: 8/1000
       }
     ];
     $scope.selectedType = angular.toJson($scope.types[0]);
     $scope.cost = null;
 
     $scope.calculate = function() {
-      $scope.cost = "$" + Math.round(($scope.loadSize * $scope.binder * angular.fromJson($scope.selectedType).value * $scope.pmt))/100;
+      $scope.litres = $scope.loadSize * $scope.binder * angular.fromJson($scope.selectedType).value;
+      $scope.cost = Math.round(($scope.litres * $scope.pmt * 100))/100;
     };
 
     $scope.calculate();
