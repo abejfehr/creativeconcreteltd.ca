0d8497ffd0a0c3393727206a8bed4c6d991702b5
Abe Fehr <abe.fehr@gmail.com>
2018-02-10 14:40:11 -0500

    Update the facebook icon to not use FontAwesome

---
+++ _site/README.md
@@ -0,0 +1,2 @@
+# [Creative Concrete](http://creativeconcreteltd.ca/)
+A Single-page HTML5 website for my brother's concrete business.
---
+++ _site/js/init.js
@@ -0,0 +1,70 @@
+/*
+  Tessellate by HTML5 UP
+  html5up.net | @n33co
+  Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+  $(function() {
+
+    var	$window = $(window);
+    var $body = $('body');
+
+    // Scrolly links.
+    $('.scrolly').scrolly();
+
+    // Gallery images
+    $('#gallery').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      gallery: {
+        enabled: true
+      }
+    });
+
+    $('.gallery-thumb').on('click', function(event) {
+      ga('send', 'event', 'button', 'click', 'gallery-thumbs', $(this).attr('href'));
+    });
+
+    $('.logix-link').on('click', function(event) {
+      ga('send', 'event', 'button', 'click', 'outbound-links', 'logix');
+    });
+
+  });
+
+
+})(jQuery);
+
+var sendEmail = function() {
+  $.ajax({
+    url: "//formspree.io/henry@creativeconcreteltd.ca",
+    method: "POST",
+    data: {
+      _replyto: $("input[name='_replyto']").val(),
+      message: $("textarea[name='message']").val(),
+      _subject: $("select[name='_subject']").val(),
+      _gotcha: $("input[name='_gotcha']").val()
+    },
+    dataType: "json"
+  });
+
+  // Clear the fields after it's been sent
+  alert('Your message was sent!');
+  $("input[name='_replyto']").val('');
+  $("textarea[name='message']").val('');
+  $("select[name='_subject']").val('');
+  $("input[type='button']").attr('disabled', true);
+};
+
+jQuery(document).scroll(function() {
+    if (jQuery(this).scrollTop() > 175) {
+        jQuery('#menu').css('position','fixed');
+        jQuery('#menu').css('top','0');
+    }
+    else {
+        jQuery('#menu').css('position','relative');
+        jQuery('#menu').css('top','');
+    }
+});
+
