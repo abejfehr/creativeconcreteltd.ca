6cb36a8ff98311f5bcdd43c752e239719ade9ca4
Abe Fehr <abe.fehr@gmail.com>
2020-10-05 23:03:55 -0400

    Better resizing

--- resize_images.sh
+++ resize_images.sh
@@ -2,10 +2,10 @@
 
 for filename in ./images/originals/*.jpg; do
   thumb=`echo $filename | sed -e "s/original/thumb/"`
-  gm convert $filename -thumbnail 300x150 $thumb
+  gm convert $filename -thumbnail 300x-1 $thumb
 done
 
 for filename in ./images/originals/*.jpg; do
   hires=`echo $filename | sed -e "s/originals/hi-res/"`
-  gm convert $filename -size 1600x1600 -resize 1600x1600 $hires
+  gm convert $filename -size 1600x-1 -resize 1600x-1 $hires
 done
