45d948ec2e355bf60a1335dbc209c2bd6f0e55ac
Abe Fehr <abe.fehr@gmail.com>
2025-02-15 12:55:21 -0500

    Keep it hidden for now

--- contact.js
+++ contact.js
@@ -1,9 +1,9 @@
-var ua = window.navigator.userAgent;
-var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
-var webkit = !!ua.match(/WebKit/i);
-var iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
+// var ua = window.navigator.userAgent;
+// var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
+// var webkit = !!ua.match(/WebKit/i);
+// var iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
 
-if (iOSSafari) {
+if (false) {
   const link = document.querySelector(".contact-link");
   link.style.display = "inline"; // the default
 }
